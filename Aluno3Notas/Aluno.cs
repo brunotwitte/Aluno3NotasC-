534ffff5b1ff1ef76bfb0dc4dd8e4e89ba8d6f73
brunotwitte <brunotwitte@gmail.com>
2023-11-27 10:35:07 -0300

    ProgramaAluno3NotasC#

---
+++ Aluno3Notas/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno3Notas {
+    class Aluno {
+
+        public String Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3 /100;
+        }
+        public bool Aprovado() {
+            if (NotaFinal() >= 60) {
+                return true;
+            }
+            else { return false; }
+
+        } public double  NotaRestante() {
+            if (Aprovado()) {
+                return 0.0;
+            }
+            else {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
